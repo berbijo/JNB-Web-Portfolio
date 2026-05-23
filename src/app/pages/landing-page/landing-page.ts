b5ab5d2cca279cf47981c7b699496dd378d3b416
berbijo <berbijo.nesc@gmail.com>
2026-05-24 06:45:59 +0800

    Added maximum budget on angular.json for building

--- src/app/pages/landing-page/landing-page.ts
+++ src/app/pages/landing-page/landing-page.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Navbar } from "../../components/navbar/navbar";
 import { Hero } from "../../components/hero/hero";
 import { Cursor } from "../../components/cursor/cursor";
-import { Traits } from "../../components/traits/traits";
 import { Experience } from "../../components/experience/experience";
 import { Projects } from "../../components/projects/projects";
 import { Footer } from "../../components/footer/footer";
@@ -10,7 +9,7 @@ import { HireMe } from "../../components/hire-me/hire-me";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Navbar, Hero, Cursor, Traits, Experience, Projects, Footer, HireMe],
+  imports: [Navbar, Hero, Cursor, Experience, Projects, Footer, HireMe],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
